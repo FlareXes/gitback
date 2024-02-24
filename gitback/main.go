542027360c5d2894bb8c70e0438a0aa39bf83070
FlareXes <70066527+FlareXes@users.noreply.github.com>
2024-02-24 23:14:51 +0530

    added git clone

--- gitback/main.go
+++ gitback/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
+	"os/exec"
 
 	"github.com/google/go-github/v59/github"
 	"github.com/joho/godotenv"
@@ -14,7 +16,7 @@ func LogResponse(resp *github.Response) {
 		fmt.Println("Token Expiration:", resp.TokenExpiration)
 	}
 
-	fmt.Println("Rate: ", resp.Rate)
+	fmt.Println("Rate:", resp.Rate)
 }
 
 func GetGitHubPAT() string {
@@ -65,8 +67,14 @@ func ListPrivateRepos() ([]*github.Repository, *github.Response) {
 	return repos, resp
 }
 
-func CloneRepo() {
+func CloneRepo(url string, outputDir string) {
+	cmd := exec.Command("git", "clone", url, outputDir)
+	output, err := cmd.CombinedOutput()
 
+	if err != nil {
+		fmt.Println("Error cloning repository:", err)
+		fmt.Println(output)
+	}
 }
 
 func GetPatUrl(fullname string) string {
@@ -77,10 +85,15 @@ func GetPatUrl(fullname string) string {
 }
 
 func main() {
+	os.Mkdir("github-repositories-backup", os.ModePerm)
 	repos, resp := ListPublicRepos("flarexes")
 
-	for i, repo := range repos {
-		fmt.Println(i, GetPatUrl(*repo.FullName))
+	for _, repo := range repos {
+		log.Println("Cloning", *repo.CloneURL)
+		outputDir := "github-repositories-backup/" + *repo.Name
+
+		// CloneRepo(GetPatUrl(*repo.FullName), outputDir)
+		CloneRepo(*repo.CloneURL, outputDir)
 	}
 
 	LogResponse(resp)
