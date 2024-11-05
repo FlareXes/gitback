e5051453986be005269877b38b0179f43713f956
FlareXes <70066527+FlareXes@users.noreply.github.com>
2024-11-05 07:40:49 +0530

    feat: pass token as argument

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ package main
 import (
 	"flag"
 	"log"
+	"os"
 
 	"github.com/flarexes/gitback/vcs"
 )
@@ -20,11 +21,23 @@ func main() {
 	noauth := flag.Bool("noauth", false, "Disable GitHub Auth, Limited to 60 Request/Hr & Public Data")
 	username := flag.String("username", "", "Required When --noauth is Set")
 	threads := flag.Int("threads", 10, "Maximum number of concurrent connections")
+	token := flag.String("token", "", "GitHub API Token")
 
 	flag.Parse()
 
 	if *noauth && *username == "" {
-		log.Fatal("error: when --noauth is set, --username is required.")
+		log.Println("Error: when --noauth is set, --username is required")
+		log.Fatal("Usage: gitback --noauth --username flarexes")
+	}
+
+	if *threads <= 1 {
+		log.Fatal("Error: --threads must be a positive integer")
+	}
+
+	if *token != "" {
+		if err := os.Setenv("GITHUB_PERSONAL_ACCESS_TOKEN", *token); err != nil {
+			log.Fatal("Error setting environment variable:", err)
+		}
 	}
 
 	vcs.Run(*noauth, *username, *threads)
